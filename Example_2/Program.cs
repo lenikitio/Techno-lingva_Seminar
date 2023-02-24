28a151b95cd70b2cc43aa480c438c32be8a25cd5
nikita <tolstoguzov.nikita98@mail.ru>
2023-02-24 17:09:15 +0300

    задача 2

---
+++ Example_2/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите номер четверти: ");
+int n = int.Parse(Console.ReadLine());
+if (n > 4 || n < 1)
+{
+    Console.WriteLine("Такой четверти не бывает");
+
+}
+else
+{
+    Console.WriteLine($"Ваша четверть: {n}");
+
+}
+if (n == 1)
+{
+    Console.WriteLine(" 0 < x < + бесконечности, 0 < y < + бесконечности");
+}
+if (n == 2)
+{
+    Console.WriteLine(" - бесконечности < x < 0, 0 < y < + бесконечности");
+}
+if (n == 3)
+{
+    Console.WriteLine(" - бесконечности < x < 0, - бесконнечности < y < 0");
+}
+if (n == 4)
+{
+    Console.WriteLine(" 0 < x < + бесконечности, - бесконнечности < y < 0");
+}
+
+
