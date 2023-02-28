edada29ac0c4051c539f68a450d361cd57062cc2
nikita <tolstoguzov.nikita98@mail.ru>
2023-02-28 13:30:01 +0300

    Задача 35

---
+++ Example_35/Program.cs
@@ -0,0 +1,38 @@
+﻿//Задайте одномерный массив из 123 случайных чисел. 
+//Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+
+Console.Clear();
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for(int i = 0; i < size; i++)
+    array[i] = new Random().Next(minValue,maxValue + 1);
+    Console.WriteLine(String.Join(", ", array));
+    return array;
+}
+
+void GetCount(int[] arr, int LeftBorder, int RightBorder)
+{
+    int count = 0;
+    foreach(int elem in arr)
+    if (elem >= LeftBorder && elem <= RightBorder)
+    count ++;
+    Console.WriteLine($"Количество элементов в массиве в вашем диапазоне: {count}");
+}
+
+Console.WriteLine("Введите количество элементов в массиве: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное значение элементов массива: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение элементов массива: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int[] NewArray = GetRandomArray(a, b, c);
+
+Console.WriteLine("Введите левую границу поиска: ");
+int d = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите правую границу поиска: ");
+int e = Convert.ToInt32(Console.ReadLine());
+
+GetCount(NewArray, d, e);
