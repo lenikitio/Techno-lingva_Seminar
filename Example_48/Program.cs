d8c4a7b78ac10bf27c5297a23c4595aa6f77d37f
nikita <tolstoguzov.nikita98@mail.ru>
2023-03-06 20:14:14 +0300

    Задача 48

---
+++ Example_48/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле:
+// Aₘₙ = m+n. Выведите полученный массив на экран.
+
+Console.Clear();
+
+Console.Write("Введите количество строк m: ");
+int strok = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов n: ");
+int stolb = Convert.ToInt32(Console.ReadLine());
+
+GetBinaryArray(strok, stolb);
+
+void GetBinaryArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            result[i, j] = i + j + 2;
+            Console.Write(result[i, j] + " ");
+        }
+    Console.WriteLine("");
+    }
+}
